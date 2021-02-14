661c1ab17a285e610329751bc3d48ef01e1c7abb
Trent Casillas <43798423+trent129@users.noreply.github.com>
2021-02-13 17:52:20 -0800

    Delete Project_4 directory

--- Project_4/README.md
+++
@@ -1,3 +0,0 @@
-Hidden Search Widget: Upon click it reveals a search bar to fill information and toggles 
-
-Future Changes: continue comments on the html and css code. Any feedback is great! Learning to how to incorporate more these projects together. 
--- Project_4/script.js
+++
@@ -1,9 +0,0 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
-
-/*Learned Toggle, it switches between adding and removing classes */
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
